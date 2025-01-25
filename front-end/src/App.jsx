8006849c175668b77353c73f27e3aeca93ffa2f8
Aniket Singh <aniketsingh90126@gmail.com>
2025-01-25 14:35:46 +0530

    feat: fotoer and nav component with styling done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,8 @@
+import './App.css';
 import React from "react";
 import Nav from "./components/Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
           <Route path="/logout" element={<h1>logout</h1>} />
           <Route path="/profile" element={<h1>profile</h1>} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
