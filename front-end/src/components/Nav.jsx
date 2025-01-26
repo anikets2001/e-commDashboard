8343f96eb2832366b9a8546641a9b322e9887d52
Aniket Singh <aniketsingh90126@gmail.com>
2025-01-26 18:30:15 +0530

    fix: navbar

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import logo from "../images/logo.jpg";
 
 const Nav = () => {
   const navigate = useNavigate;
   const auth = localStorage.getItem("user");
+  const { name } = JSON.parse(auth);
 
   const handleLogout = () => {
     localStorage.clear();
@@ -11,32 +13,31 @@ const Nav = () => {
   };
   return (
     <div className="navWrapper">
-      <Link to={"/"} className="navItem">
-        Homepage
-      </Link>
-      <Link to={"/add"} className="navItem">
-        Add Product
-      </Link>
-      <Link to={"/update"} className="navItem">
-        Update Product
-      </Link>
-      <Link to={"/profile"} className="navItem">
-        Profile
-      </Link>
-      {/* {auth ? (
-        <Link to={"/signUp"} className="navItem" onClick={handleLogout}>
-          Logout
-        </Link>
-      ) : (
-        <Link to={"/signUp"} className="navItem">
-          Sign up
-        </Link>
-      )} */}
-
       {auth ? (
-        <Link to={"/signUp"} className="navItem" onClick={handleLogout}>
-          Logout
-        </Link>
+        <>
+          <img
+            src={logo}
+            alt="logo"
+            height={40}
+            width={40}
+            style={{ borderRadius: "50%" }}
+          />
+          <Link to={"/"} className="navItem">
+            Home
+          </Link>
+          <Link to={"/add"} className="navItem">
+            Add Product
+          </Link>
+          <Link to={"/update"} className="navItem">
+            Update Product
+          </Link>
+          <Link to={"/profile"} className="navItem">
+            Profile
+          </Link>
+          <Link to={"/signUp"} className="navItem" onClick={handleLogout}>
+            Logout ({name})
+          </Link>
+        </>
       ) : (
         <>
           <Link to={"/signUp"} className="navItem">
